d301b48257b5af1f91a043ecd51ba4fcf18fb8f9
achuthaganiga <achuthaganiga@gmail.com>
2024-04-18 23:29:52 +0530

    error testing

--- KesariMitayiAPI/Program.cs
+++ KesariMitayiAPI/Program.cs
@@ -12,7 +12,7 @@ namespace KesariMitayiAPI
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen()
 
             var app = builder.Build();
 
